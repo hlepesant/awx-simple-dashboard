6e6e840505be7ebdec24e03ff0f07d3255170b1e
Hugues Lepesant <hugues@lepesant.com>
2022-02-14 14:55:39 +0100

    fix appi/url url

--- src/Service/AwxGenerator.php
+++ src/Service/AwxGenerator.php
@@ -6,11 +6,14 @@ use Symfony\Component\HttpClient\HttpClient;
 
 Class AwxGenerator
 {
+    private $awx_url;
+    private $awx_api;
     private $token;
 
-    public function __construct(string $awx_url, string $token)
+    public function __construct(string $awx_url, string $awx_api, string $token)
     {
 	      $this->awx_url = $awx_url;
+	      $this->awx_api = $awx_api;
         $this->token = $token;
     }
 
@@ -20,7 +23,7 @@ Class AwxGenerator
             'auth_bearer' => $this->token,
         ]); 
 
-        $response = $client->request('GET', $this->awx_url.'/api/v2/jobs', [
+        $response = $client->request('GET', $this->awx_api.'/api/v2/jobs', [
             'query' => [
                 'order_by' => sprintf('%s', $sorted_by),
                 'page_size' => $page_size,
